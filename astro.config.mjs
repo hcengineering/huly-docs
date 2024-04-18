c6e1f650572b6c3b8a7907d347bfbb0e625d388e
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-04-18 09:34:44 -0400

    Add GIF to Todos doc, configure image settings to remove file size restriction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,49 +1,79 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			customCss: ['./src/styles.css'],
-			title: 'Huly Docs',
-			social: {
-				github: 'https://github.com/hcengineering/platform',
-			},
-			sidebar: [
-				{
-					label: 'Getting Started',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'What is Huly?', link: '/getting-started/introduction/' },
-						{ label: 'Workspace Setup', link: '/getting-started/workspace-setup/' },
-						{ label: 'Create a Project', link: '/getting-started/create-project/' },
-						{ label: 'Create an Issue', link: '/getting-started/create-issue/' },
-						{ label: 'Support', link: '/getting-started/support/' },
-					],
-				},
-				{
-					label: 'Team Workflows',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Issues', link: '/team-workflows/issues/' },
-						{ label: 'Todos', link: '/team-workflows/todos/' },
-						{ label: 'Chat', link: '/team-workflows/chat/' },
-						{ label: 'Virtual Office', link: '/team-workflows/virtual-office/' },
-						{ label: 'Inbox', link: '/team-workflows/inbox/' },
-						{ label: 'Documents', link: '/team-workflows/documents/' },
-					],
-				},
-				{
-					label: 'Integrations',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Google-Calendar', link: '/team-workflows/google-calendar/' },
-						{ label: 'Gmail', link: '/team-workflows/gmail/' },
-						{ label: 'GitHub', link: '/team-workflows/github/' },
-					],
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    customCss: ['./src/styles.css'],
+    title: 'Huly Docs',
+    social: {
+      github: 'https://github.com/hcengineering/platform'
+    },
+    sidebar: [{
+      label: 'Getting Started',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'What is Huly?',
+        link: '/getting-started/introduction/'
+      }, {
+        label: 'Workspace Setup',
+        link: '/getting-started/workspace-setup/'
+      }, {
+        label: 'Create a Project',
+        link: '/getting-started/create-project/'
+      }, {
+        label: 'Create an Issue',
+        link: '/getting-started/create-issue/'
+      }, {
+        label: 'Support',
+        link: '/getting-started/support/'
+      }]
+    }, {
+      label: 'Team Workflows',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Issues',
+        link: '/team-workflows/issues/'
+      }, {
+        label: 'Todos',
+        link: '/team-workflows/todos/'
+      }, {
+        label: 'Chat',
+        link: '/team-workflows/chat/'
+      }, {
+        label: 'Virtual Office',
+        link: '/team-workflows/virtual-office/'
+      }, {
+        label: 'Inbox',
+        link: '/team-workflows/inbox/'
+      }, {
+        label: 'Documents',
+        link: '/team-workflows/documents/'
+      }]
+    }, {
+      label: 'Integrations',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Google-Calendar',
+        link: '/team-workflows/google-calendar/'
+      }, {
+        label: 'Gmail',
+        link: '/team-workflows/gmail/'
+      }, {
+        label: 'GitHub',
+        link: '/team-workflows/github/'
+      }]
+    }]
+  })],
+  image: {
+    service: {
+      config: {
+        limitInputPixels: false
+      }
+    }
+  }
 });
--- src/content/docs/team-workflows/todos.md
+++
@@ -1,4 +0,0 @@
----
-title: Todos
-description: tbd
----
