82332aa9962c21ea6e255bab5baf08345caded4c
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-07-31 07:46:53 -0400

    Change todos to action items everywhere in docs
    Signed-off-by: Abigail Dawson <abigaildawson.dev@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,11 +37,11 @@ export default defineConfig({
           label: 'Viewing and sorting issues',
           link: '/task-tracking/viewing-issues/'
         }, {
-          label: 'Creating todos',
-          link: '/task-tracking/creating-todos/'
+          label: 'Creating action items',
+          link: '/task-tracking/creating-action-items/'
         }, {
-          label: 'Scheduling todos',
-          link: '/task-tracking/scheduling-todos/'
+          label: 'Scheduling action items',
+          link: '/task-tracking/scheduling-action-items/'
         },
       ]
     }, {
@@ -54,8 +54,8 @@ export default defineConfig({
           label: 'Collaborative editing',
           link: '/knowledge-management/collaborative-editing/'
         },  {
-          label: 'Todos in documents',
-          link: '/knowledge-management/documents-todos/'
+          label: 'Action items in documents',
+          link: '/knowledge-management/documents-action-items/'
         },  {
           label: 'Drive',
           link: '/knowledge-management/drive/'
